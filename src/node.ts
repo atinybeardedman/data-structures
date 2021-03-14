31f8512cb731b7e5bc7098ff3affe211a72bf19c
Sean Dickinson <contact@seandickinson.dev>
2021-03-13 20:16:50 -0500

    fix: linked node classes references updated

--- src/node.ts
+++ src/node.ts
@@ -1,42 +1,47 @@
+/**
+ * An implementation of a node that contains a single data property
+ */
 export class Node<T> {
     private _data: T;
-    constructor(data: T){
-        this._data = data;
+    constructor(data: T) {
+      this._data = data;
     }
 
     get data(): T {
-        return this._data;
+      return this._data;
     }
 
     set data(value: T) {
-        this._data = value;
+      this._data = value;
     }
 }
 
+/**
+ * An implementation of a node that contains a data property 
+ */
 export class LinkedNode<T> extends Node<T> {
-    private _next: Node<T> | null;
-    private _previous: Node<T> | null;
-    constructor(data: T){
-        super(data);
-        this._next = null;
-        this._previous = null;
+    private _next: LinkedNode<T> | null;
+    private _previous: LinkedNode<T> | null;
+    constructor(data: T) {
+      super(data);
+      this._next = null;
+      this._previous = null;
     }
 
-    get next(): Node<T> | null {
-        return this._next;
+    get next(): LinkedNode<T> | null {
+      return this._next;
     }
 
-    set next(node: Node<T> | null) {
-        this._next = node;
+    set next(node: LinkedNode<T> | null) {
+      this._next = node;
     }
 
-    get previous(): Node<T> | null {
-        return this._previous;
+    get previous(): LinkedNode<T> | null {
+      return this._previous;
     }
 
-    set previous(node: Node<T> | null) {
-        this._previous = node;
+    set previous(node: LinkedNode<T> | null) {
+      this._previous = node;
     }
 
-    
 }
